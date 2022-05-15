ce12b0e74d740bc304534d21dbf84b79d9ac8809
Stephen Paynter <52934144+stephenpaynter@users.noreply.github.com>
2022-05-15 17:01:57 +0100

    Create get_light_status.py

---
+++ get_light_status.py
@@ -0,0 +1,13 @@
+#Dirty mock up using light id 8
+
+import requests
+import json
+
+response = requests.get("http://192.168.1.86/api/cYrogcQrJAv-zqaaoNjBRljNIMF3j8AQ52F-tOBC/lights/8")
+
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+jprint(response.json())
