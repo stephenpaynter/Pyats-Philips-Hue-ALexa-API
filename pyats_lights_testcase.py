28b7a18daf55954fc18f17069697b4f2dcef59b6
Stephen Paynter <52934144+stephenpaynter@users.noreply.github.com>
2022-05-17 14:36:31 +0100

    Update pyats_lights_testcase.py

--- pyats_lights_testcase.py
+++ pyats_lights_testcase.py
@@ -4,10 +4,17 @@ from genie.testbed import load
 import requests
 import json
 
-url = "http://192.168.1.86/api/cYrogcQrJAv-zqaaoNjBRljNIMF3j8AQ52F-tOBC/lights/9/state"
+_light_number = 9
+
+url = f'http://192.168.1.86/api/cYrogcQrJAv-zqaaoNjBRljNIMF3j8AQ52F-tOBC/lights/{_light_number}/state'
+
+print (url)
 
 _data_on_red = {"on":True, "sat":254, "bri":254,"hue":5000,"ct": 500, "xy": [0.6792, 0.3038]}
 _data_on_green = {"on":True, "sat":254, "bri":254,"hue":5000,"ct": 500, "xy": [0.2688, 0.6171]}
+_data_off = {"on":False}
+
+r = requests.put(url, json.dumps(_data_off), timeout=5)
 
 _testbed = load('/Repositories/1-Ansible Code/testbed.yml') 
 _device = _testbed.devices['R1']
@@ -15,6 +22,7 @@ _device.connect()
 _routes = _device.parse('show ip route') 
 
 print(_routes)
+
 
 _routecheck = len(_routes['vrf']['default']['address_family']['ipv4']['routes']) 
 
